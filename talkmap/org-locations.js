27b2a262ef6230460987cb45813a31a1934cf664
Jiaye Wu <161971145+jiaye-wu@users.noreply.github.com>
2024-03-05 20:45:44 +0100

    Update Talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,13 +5,28 @@ var addressPoints = [
     113.2592945
   ],
   [
+    "Sydney, NSW, Australia",
+    -33.8698439,
+    151.2082848
+  ],
+  [
+    "Macau, China",
+    22.1899448,
+    113.5380454
+  ],
+  [
+    "San Jose, California, United States",
+    37.3361663,
+    -121.890591
+  ],
+  [
     "Zurich, Switzerland",
     47.3744489,
     8.5410422
   ],
   [
     "Shanghai, China",
-    31.2312707,
-    121.4700152
+    31.2323437,
+    121.4691024
   ]
 ];
