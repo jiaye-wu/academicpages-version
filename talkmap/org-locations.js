b75b83bb658787cf6ae53c8242bb5f152c53c48b
Jiaye Wu <161971145+jiaye-wu@users.noreply.github.com>
2024-03-05 19:55:02 +0100

    Update Talk Map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Guangzhou, China",
+    23.1301964,
+    113.2592945
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Shanghai, China",
+    31.2312707,
+    121.4700152
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Zurich, Switzerland",
+    47.3744489,
+    8.5410422
   ]
 ];
