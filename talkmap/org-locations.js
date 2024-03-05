9eef417191756f23164be7a2f201241f23776c55
Jiaye Wu <161971145+jiaye-wu@users.noreply.github.com>
2024-03-05 20:07:51 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,13 +5,13 @@ var addressPoints = [
     113.2592945
   ],
   [
-    "Shanghai, China",
-    31.2312707,
-    121.4700152
-  ],
-  [
     "Zurich, Switzerland",
     47.3744489,
     8.5410422
+  ],
+  [
+    "Shanghai, China",
+    31.2312707,
+    121.4700152
   ]
 ];
